d66261cef09207f8510f2adfaa61ff7a77075d6d
MakovskyiNikolai <makovskyinikolai@gmail.com>
2018-10-04 11:26:49 +0300

    Commit 4 - WhileLoops

---
+++ src/WhileLoops.java
@@ -0,0 +1,17 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+        //boolean t = 5>=5;
+        //boolean t = 5==5;  //операция сравнивания  - true
+        //boolean t = 5==1;     //операция сравнивания  - false
+        //int value = 0;
+       //boolean t = value>5;
+
+
+        int value = 0;
+        while (value<5){
+            System.out.println("Hello "+ value);
+            value = value+1;
+        }
+    }
+}
+
