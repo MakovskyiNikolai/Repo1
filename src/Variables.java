ac3101d337e60cc2d9b6465eba97fa28b27204a7
MakovskyiNikolai <makovskyinikolai@gmail.com>
2018-10-04 11:08:57 +0300

    Commit 2 - Variables

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+          short myShort = 3266;
+          long myLong = 265515151;
+
+          double myDouble = 235.35;
+          float myFloat = 2363.4f;
+
+          char c = 'a';
+          boolean b = true;
+
+          byte myByte = 100;   // -128 до 127
+        System.out.println(myInt);
+          }
+}
