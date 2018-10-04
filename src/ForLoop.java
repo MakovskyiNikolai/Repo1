ecb0e0b3f9cb0f6736616148d128a9779a3c5d09
MakovskyiNikolai <makovskyinikolai@gmail.com>
2018-10-04 12:01:09 +0300

    Commit 5 - For Loops

---
+++ src/ForLoop.java
@@ -0,0 +1,8 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0;i<=10;i = i+5){           //for (int i = 0;i<=10;i = i+1){
+            //for (int i = 10;i>=0;i = i-5){
+            System.out.println("Hello "+ i);    // i++ == i = i+1
+        }                                      // i++ полностью инентичен i = i+1
+    }
+}
